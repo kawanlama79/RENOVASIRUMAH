c5958f6cef28cc22d1624d5fc51915c5be7199a8
kawanlama79 <k6502907@gmail.com>
2025-11-06 14:20:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// JavaScript untuk fitur interaktif sederhana jika diperlukan
+document.querySelector('.cta').addEventListener('click', function() {
+    alert("Terima kasih telah menghubungi kami! Kami akan segera menghubungi Anda.");
+});
 
